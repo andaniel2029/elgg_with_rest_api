dd1ef1d94bcb7cb5f033e6422b9ead77b9ab555e
andaniel2029 <51064752+andaniel2029@users.noreply.github.com>
2019-05-31 01:34:08 +0700

    album update

--- lib/album.php
+++ lib/album.php
@@ -2,7 +2,7 @@
 /**
  * Created by IntelliJ IDEA.
  * User: Daniel
- * Date: 2/11/2016
+ * Date: 5/30/2019
  * Time: 4:40 PM
  */
 
@@ -21,7 +21,7 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
     $loginUser = elgg_get_logged_in_user_entity();
 
     if($context == "all"){
-        $params = array(
+        $params = [
             'type' => 'object',
             'subtype' => 'album',
             'owner_guid' => NULL,
@@ -30,9 +30,9 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
             'full_view' => false,
             'list_type' => 'gallery',
             'gallery_class' => 'tidypics-gallery'
-        );
+        ];
     } else if ($context == 'mine') {
-        $params = array(
+        $params = [
             'type' => 'object',
             'subtype' => 'album',
             'owner_guid' => $user->guid,
@@ -41,15 +41,15 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
             'full_view' => false,
             'list_type' => 'gallery',
             'gallery_class' => 'tidypics-gallery'
-        );
+        ];
     } else if ($context == 'friends') {
-        if ($friends = $user->getFriends(array('limit' => false))) {
-            $friendguids = array();
+        if ($friends = $user->getFriends(['limit' => false])) {
+            $friendguids = [];
             foreach ($friends as $friend) {
                 $friendguids[] = $friend->getGUID();
             }
 
-            $params = array(
+            $params = [
                 'type' => 'object',
                 'subtype' => 'album',
                 'owner_guids' => $friendguids,
@@ -58,10 +58,10 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
                 'full_view' => false,
                 'list_type' => 'gallery',
                 'gallery_class' => 'tidypics-gallery'
-            );
+            ];
         }
     } else {
-        $params = array(
+        $params = [
             'type' => 'object',
             'subtype' => 'album',
             'owner_guid' => NULL,
@@ -70,15 +70,14 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
             'full_view' => false,
             'list_type' => 'gallery',
             'gallery_class' => 'tidypics-gallery'
-        );
+        ];
     }
 
     $albums = elgg_get_entities($params);
 
-
     $site_url = get_config('wwwroot');
     if($albums) {
-        $return = array();
+        $return = [];
         foreach($albums as $single ) {
             $album['guid'] = $single->guid;
             $album_cover = $single->getCoverImage();
@@ -91,12 +90,10 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
             $icon_file_name = substr_replace($file_name, 'smallthumb', $position, 0);
 
             $image_icon_url = $site_url . 'services/api/rest/json/?method=image.get_post';
-            $image_icon_url = $image_icon_url . '&joindate=' . $image_join_date . '&guid=' . $album_cover->guid
-                . '&name=' . $icon_file_name;
+            $image_icon_url = $image_icon_url . '&joindate=' . $image_join_date . '&guid=' . $album_cover->guid . '&name=' . $icon_file_name;
 
             $image_url = $site_url . 'services/api/rest/json/?method=image.get_post';
-            $image_url = $image_url . '&joindate=' . $image_join_date . '&guid=' . $album_cover->guid
-                . '&name=' . $file_name;
+            $image_url = $image_url . '&joindate=' . $image_join_date . '&guid=' . $album_cover->guid . '&name=' . $file_name;
 
             $album['cover_icon_url'] = $image_icon_url;
             $album['cover_image_url'] = $image_url;
@@ -121,16 +118,12 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
                 $album['description'] = '';
             }
 
-
-
             $owner = get_entity($single->owner_guid);
             $album['owner']['guid'] = $owner->guid;
             $album['owner']['name'] = $owner->name;
             $album['owner']['username'] = $owner->username;
-            $album['owner']['avatar_url'] = getProfileIcon($owner); //$owner->getIconURL('small');
+            $album['owner']['avatar_url'] = getProfileIcon($owner);
 
-
-//            $photo['img_url'] = $img_url;
             $album['like_count'] = likes_count_number_of_likes($single->guid);
             $album['comment_count'] = api_get_image_comment_count($single->guid);
             $album['like'] = checkLike($single->guid, $loginUser->guid);
@@ -143,17 +136,17 @@ function album_get_posts($context,  $limit = 20, $offset = 0, $username) {
         throw new InvalidParameterException($msg);
     }
 
-
     return $return;
 }
 
 elgg_ws_expose_function('album.get_posts',
     "album_get_posts",
-    array(	'context' => array ('type' => 'string'),
-        'limit' => array ('type' => 'int', 'required' => false, 'default' => 20),
-        'offset' => array ('type' => 'int', 'required' => false, 'default' => 0),
-        'username' => array ('type' => 'string', 'required' => false),
-    ),
+    [
+        'context'   => ['type' => 'string'],
+        'limit'     => ['type' => 'int', 'required' => false, 'default' => 20],
+        'offset'    => ['type' => 'int', 'required' => false, 'default' => 0],
+        'username'  => ['type' => 'string', 'required' => false],
+    ],
     "GET all the albums",
     'GET',
     true,
